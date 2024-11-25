error fix about gradient accumulate

coco_dataset.py
@@ -106,42 +106,47 @@ class CocoDataset(Dataset):
 
 
 class PseudoLabelDataset(Dataset):
-    def __init__(self, coco_dataset: CocoDataset, strong_transforms, model, device, threshold, decay=0.99):
+    def __init__(self, coco_dataset: CocoDataset, strong_transforms, model, device, threshold, mode="fusion"):
         self.coco_dataset = coco_dataset
         self.device = device
         self.model = model
         self.threshold = threshold
         self.strong_transforms = strong_transforms
-        self.decay = decay
         self.reversible_augmentation = get_reversible_augmentation()
         self.history_targets = {}
+        if mode not in ["fusion", "once", "real_time"]:
+            raise ValueError('mode value can only be "fusion", "once" or "real_time"')
+        self.mode = mode
 
     def __len__(self):
         return len(self.coco_dataset)
 
-    def target_fusion(self, idx, boxes, labels, scores):
+    def set_history(self, idx, boxes, labels, scores):
+        self.history_targets[idx] = (boxes.clone(), labels.clone(), scores.clone())
+
+    def history_fusion(self, idx, boxes, labels, scores):
         if idx in self.history_targets.keys():
             history_boxes, history_labels, history_scores = self.history_targets[idx]
-            history_scores *= self.decay
 
             boxes = torch.cat([history_boxes, boxes], dim=0)
             labels = torch.cat([history_labels, labels], dim=0)
             scores = torch.cat([history_scores, scores], dim=0)
 
-            keep = scores >= self.threshold
-            boxes, labels, scores = boxes[keep], labels[keep], scores[keep]
-
             keep = box_ops.batched_nms(boxes, scores, labels, 0.5)
             boxes, labels, scores = boxes[keep], labels[keep], scores[keep]
-
         self.history_targets[idx] = (boxes.clone(), labels.clone(), scores.clone())
         return boxes, labels, scores
 
-    def __getitem__(self, idx):
-        self.model.eval()
-        img, _ = self.coco_dataset[idx]
-        img = F.to_dtype(img, torch.float, scale=True)
+    def have_history(self, idx):
+        return idx in self.history_targets.keys()
 
+    def get_history(self, idx):
+        if idx in self.history_targets.keys():
+            history_boxes, history_labels, history_scores, fusion_count = self.history_targets[idx]
+            return history_boxes, history_labels, history_scores
+
+    def generate_pseudo_label(self, img):
+        self.model.eval()
         with torch.no_grad():
             aug_img, undo_action = self.reversible_augmentation.apply(img)
             preds = self.model([aug_img.to(self.device)])[0]
@@ -149,18 +154,32 @@ class PseudoLabelDataset(Dataset):
             boxes = self.reversible_augmentation.undo(boxes, undo_action)
             keep = scores >= self.threshold
             boxes, labels, scores = boxes[keep], labels[keep], scores[keep]
-            boxes, labels, scores = self.target_fusion(idx, boxes, labels, scores)
-            target = {
+            return boxes, labels, scores
+
+    def __getitem__(self, idx):
+        self.model.eval()
+        img, _ = self.coco_dataset[idx]
+        img = F.to_dtype(img, torch.float, scale=True)
+        if self.mode == "once":
+            if self.have_history(idx):
+                boxes, labels, scores = self.get_history(img)
+            else:
+                boxes, labels, scores = self.generate_pseudo_label(img)
+                self.set_history(idx, boxes, labels, scores)
+        else:
+            boxes, labels, scores = self.generate_pseudo_label(img)
+            if self.mode == "fusion":
+                boxes, labels, scores = self.history_fusion(idx, boxes, labels, scores)
+
+        target = {
                 "supervised": False,
                 "labels": labels,
                 "boxes": BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img)),
                 "scores": scores
             }
-            if idx % 1000 == 0:
-                plot.plot_data(img, target, global_config.CLASSES, "runtime/label", f"{idx}.png")
-
-            img, target = self.strong_transforms(img, target)
-
+        if idx % 1000 == 0:
+            plot.plot_data(img, target, global_config.CLASSES, "runtime/label", f"{idx}.png")
+        img, target = self.strong_transforms(img, target)
         return img, target
 
 

global_config.py
@@ -8,18 +8,16 @@ DETECTION_MODEL = fasterrcnn_resnet50_fpn_v2
 DEVICE = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
 TRAIN_BATCH_SIZE = 4
-ACCUMULATION_STEPS = 1
-
+ACCUMULATION_STEPS = 2
 
 EVAL_BATCH_SIZE = 6
 CHECKPOINT_FREQ = 50  # set checkpoint every x epochs
 
-
-TRAIN_STATE_PRINT_FREQ = 250  # print train state every x iters
-SUPERVISED_EVAL_FREQ = 5000  # eval every x iters
+TRAIN_STATE_PRINT_FREQ = 500  # print train state every x iters
+SUPERVISED_EVAL_FREQ = 2000  # eval every x iters
 SEMI_SUPERVISED_EVAL_FREQ = 2000  # eval every x iters
 
-MODEL_STORAGE = "model_storage"  # the folder for model saving
+MODEL_STORAGE = "../autodl-fs/model_storage"  # the folder for model saving
 
 # Coco2014 Dataset
 CLASSES = ['background', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',